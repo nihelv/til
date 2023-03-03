cf51f76d28c9849076aa4c12b08533c03962c7b5
nihelv <ys.chu0601@gmail.com>
2023-03-03 19:19:50 +0900

    update html teamproject

--- Cording Test/01.11 swea.py
+++ Cording Test/01.11 swea.py
@@ -1,77 +1,18 @@
-# 2047. 신문 헤드라인
-
-h=input()
-# 헤드라인 문자열 입력
-# 공백은 무조건 언더스코어로 입력하는 규칙인가?
-
-result=""
-
-for i in h:
-    if i.islower() == True :
-        w=i.upper()
-    else:
-            w=i
-    result += w
-
-print(result)
 
 
-# 2025. N줄덧셈
-
-a=int(input())
-b=range(1, a+1)
-
-print(sum(b))
-
-
-# 1936. 1대1 가위바위보
-
-p1, p2 = list(map(int,input().split()))
-l1=[p1, p2]
-
-for g in l1:
-    if (p1 == (p2+1)%3) :
-        print("A")
-    elif (p1 != (p2+1)%3) :
-        print("B")
-    else:
-        print('error')
+numer1=1
+denom1=2
+numer2=3
+denom2=4
 
 
 
-# 2027. 대각선 출력하기
+# def solution(numer1, denom1, numer2, denom2):
+#     answer = []
+    
+if denom1 == 0 or denom2 == 0:
+    raise ValueError("Denominators cannot be zero")
 
-for i in range(5) :
-    for j in range(5) :
-        if i == j :
-            print('#', end='')
-        else :
-            print('+', end='')
-    print()
+result = Fraction(numer1, denom1) + Fraction(numer2, denom2)
 
-
-
-# 2058 자릿수 더하기
-
-k = int(input())
-sum = 0
-for i in range(0, 4):
-    if k <= 0:
-        break
-    j = k % 10
-    k = int(k / 10)
-    sum = sum + j
-print(sum)
-
-
-# 2019 더블더블
-
-a = int(input())
-for i in range(0, a+1):
-    print(2**i,end=' ')
-
-
-
-
-
-
+answer=[result.numerator, result.denominator]
